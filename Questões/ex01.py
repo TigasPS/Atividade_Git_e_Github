bd7da5182bd36754a90dffea790af4534c07375b
Tiago Santos <santospinheirotiago@gmail.com>
2026-06-23 11:27:08 -0300

    Resolução: ex01.py

---
+++ Questões/ex01.py
@@ -0,0 +1,11 @@
+'''
+1 - Defina uma função que leia do teclado uma sequência de números inteiros dados 
+em uma única linha. A função deve retornar uma lista contendo os números que 
+estão na linha.
+'''
+
+def ler_numeros ():
+    lista = [int(x) for x in input().split()]
+    return lista
+resultado = ler_numeros()
+print (resultado)
