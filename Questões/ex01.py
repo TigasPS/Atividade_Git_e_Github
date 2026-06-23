8dcf1930562b105d318c2c02b880731a67914506
Tiago Santos <santospinheirotiago@gmail.com>
2026-06-23 16:41:06 -0300

    correção ex01.py

--- Questões/ex01.py
+++ Questões/ex01.py
@@ -1,5 +1,5 @@
 '''
-1 - Defina uma função que leia do teclado uma sequência de números inteiros dados 
+01 - Defina uma função que leia do teclado uma sequência de números inteiros dados 
 em uma única linha. A função deve retornar uma lista contendo os números que 
 estão na linha.
 '''
