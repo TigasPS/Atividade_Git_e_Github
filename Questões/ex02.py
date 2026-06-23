d4d4b19958070b9ffac46f2ef4da0ea9e78ad7ec
Tiago Santos <santospinheirotiago@gmail.com>
2026-06-23 16:41:27 -0300

    correção ex03.py

--- Questões/ex02.py
+++ Questões/ex02.py
@@ -1,5 +1,5 @@
 '''
-2 - Defina a função "descendentes" que pega uma árvore genealógica e retorna 
+02 - Defina a função "descendentes" que pega uma árvore genealógica e retorna 
 todos os descendentes da raiz. Utilize compreensões.
 '''
 
