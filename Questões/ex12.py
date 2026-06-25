d842513cf83d0486171e4a7a60633582f0576216
Tiago Santos <santospinheirotiago@gmail.com>
2026-06-25 08:52:48 -0300

    Update ex12.py

--- Questões/ex12.py
+++ Questões/ex12.py
@@ -1,7 +1,7 @@
 '''
 12 - Crie uma lista que que extraia todas as letras das palavras 
 e gere uma única lista. Ex: palavras = ["python", "java"]. 
-Saída = ['p', 'y', 't', 'h', 'o', 'n', 'j', 'a', 'v', 'a']
+Saída = ['p', 'y', 't', 'h', 'o', 'n', 'j', 'a', 'v', 'a'].
 '''
 def separador (x):
     return [y for i in x for y in i]
