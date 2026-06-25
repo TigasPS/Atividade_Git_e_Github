342fcfd6493af110143ed94d9a4f9d7aaafc1fb6
Tiago Santos <santospinheirotiago@gmail.com>
2026-06-25 14:30:49 -0300

    Resolução: ex09.py

--- Questões/ex09.py
+++ Questões/ex09.py
@@ -1,3 +1,10 @@
 '''
 09 - Crie uma lista com todos os números primos de 1 a n.
 '''
+def primo(n):
+    return len([c for c in range(1, n + 1) if n % c == 0]) == 2
+def lista(n):
+
+    return [x for x in range(1, n + 1) if primo(x)]
+
+print(lista(int(input())))
