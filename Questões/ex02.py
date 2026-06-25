13f3ea4126ac0ae71ef1bef9811511a97d2702ae
Tiago Santos <santospinheirotiago@gmail.com>
2026-06-25 15:00:59 -0300

    Resolução: ex02.py

--- Questões/ex02.py
+++ Questões/ex02.py
@@ -3,4 +3,19 @@
 todos os descendentes da raiz. Utilize compreensões.
 '''
 
+def descendentes(arvore):
+    nome, filhos = arvore
+    return [filho[0] for filho in filhos] + [d for filho in filhos for d in descendentes(filho)]
+
+
+arvore = ("João", [
+    ("Maria", [
+        ("Ana", []),
+        ("Pedro", [])
+    ]),
+    ("Carlos", [])
+])
+
+print(descendentes(arvore))
+
     
