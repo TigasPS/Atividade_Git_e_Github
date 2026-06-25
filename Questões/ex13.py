750848d28de12a04cfe4617b4640ea5ca7a63bcd
Tiago Santos <santospinheirotiago@gmail.com>
2026-06-25 14:35:49 -0300

    Resolução: ex13.py

--- Questões/ex13.py
+++ Questões/ex13.py
@@ -1,3 +1,7 @@
 '''
 13 - Dada uma matriz de inteiros, me faça uma lista com apenas os numeros pares da matriz.
 '''
+def pares_da_matriz (m):
+    return [i for n in m for i in n if i%2==0]
+
+print(pares_da_matriz([[1,2,3,4],[5,6,7,8]]))
